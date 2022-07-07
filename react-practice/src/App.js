cea157c6a81938ca4f7b0a4a585990378e0bb9e2
Edgar Chmylko <echmylko@gmail.com>
2022-07-07 14:46:02 +0300

    .

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
+import Home from './components/pages/HomePage/Home';
+import Footer from './components/pages/Footer/Footer';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import { Route, Routes } from 'react-router-dom';
+import SignUp from './components/pages/SignUp/SignUp';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+        <Routes>     
+          <Route path="/" element={<Home />} />
+          <Route path='/products' element={<Products />}/>
+          <Route path='/services' element={<Services />}/>
+          <Route path='/sign-up' element={<SignUp />}/>
+        </Routes>
+      <Footer />
+    </>
   );
 }
 
